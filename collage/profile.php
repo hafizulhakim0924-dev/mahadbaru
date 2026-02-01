dabe203126b9c7e3539235a50908d1c8d545e9ae
Hakim <hafizul.hakim0924@gmail.com>
2026-02-01 11:57:10 +0700

    terbaru

--- collage/profile.php
+++ collage/profile.php
@@ -942,7 +942,7 @@ $show_menu = empty($current_tab);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portal Pembayaran Mahad Ibnu Zubair</title>
+    <title>Portal Pembayaran Mahad Ibnu Zubair Padang</title>
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/qrious/4.0.2/qrious.min.js"></script>
     
